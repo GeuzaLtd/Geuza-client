12bb161a06cf20f7c4241d9eecb678d48d20f61f
Toussaint-Manzi <toussaintmanzi006@gmail.com>
2025-07-08 14:47:28 +0200

    added dashboard layout

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 import { useDispatch } from "react-redux";
-import { logout } from "../../redux/features/authSlice";
+import { logout } from "../../../redux/features/authSlice";
 import { useRouter } from "next/navigation";
 
 export default function DashboardPage() {
   return (
     <div className="w-full h-full bg-[#F5F6FA] p-8">
-      <h1 className="text-2xl font-bold">Welcome to the Admin Dashboard</h1>
-      <p className="mt-4">This is the main dashboard page.</p>
+      <h1 className="text-2xl font-bold">This is Product Page</h1>
+      <p className="mt-4">This is the main product page.</p>
     </div>
   );
 }
