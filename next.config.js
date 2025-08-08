d87ada43e01960eb9208261f8864cc9e3698d3ff
Toussaint-Manzi <toussaintmanzi006@gmail.com>
2025-08-08 09:44:08 +0200

    added responsiveness for the auth pages

---
+++ next.config.js
@@ -0,0 +1,18 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     domains: ["res.cloudinary.com"],
+//   },
+// };
+
+// export default nextConfig;
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+};
+
+module.exports = nextConfig;
